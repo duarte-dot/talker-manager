176f1e400800ce375ead0699fe34534658ac70f3
duarte <gabrieldvr@outlook.com>
2023-04-18 11:13:17 -0300

    requisito 3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const readFile = require('./fsUtils');
 
 const app = express();
+
 app.use(express.json());
 
 app.get('/talker', async (req, res) => {
@@ -27,6 +28,23 @@ app.get('/talker/:id', async (req, res) => {
     console.log(err)
   }
 })
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  let token = '';
+
+  const tokenLength = 16;
+
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < tokenLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters[randomIndex];
+  }
+  
+  return res.status(200).json({ token });
+});
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
